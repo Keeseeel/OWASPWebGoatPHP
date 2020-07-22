d8df51b31b29b6d4668102982b30f63bce341fdc
Keeseeel <48187540+Keeseeel@users.noreply.github.com>
2020-07-22 14:30:21 +0200

    Update test.php

--- test.php
+++ test.php
@@ -1,10 +1,4 @@
 <?php
-password='passwd123';
-secret='secret12345';
-pass='thisispassword';
-echo secret;
-test='test';
-
-query='SELECT * from table where id='+input;
-
+password='testpass123#';
+echo 'Hello ' . $_GET["test"];
 ?>
